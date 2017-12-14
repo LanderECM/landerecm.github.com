613a38c6be37fda549e47570222388a101a4ef7d
LanderECM <34285205+LanderECM@users.noreply.github.com>
2017-12-14 15:26:07 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,9 @@
+window.onload = function (){
+var x1 = document.getElementById("x1");
+x1.innerHTML = 0;
+var x2 = document.getElementById("x2");
+x2.innerHTML = 0;
+var y1 = document.getElementById("y1");
 y1.innerHTML = 0;
 var y2 = document.getElementById("y2");
 y2.innerHTML = 0;
